618fadba537b34c740e1c4252fc9a0d11fc89e9a
AYUSH PATNI <ayushdpatni@gmail.com>
2023-05-09 19:26:41 +0530

    random

--- book store/config.php
+++ book store/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','Patni@1008','shop_db') or die('connection failed');
 
 ?>
